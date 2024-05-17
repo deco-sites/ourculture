cc5f9a46061985c51125bb7d229485e78d625867
junielson_tardin_corebiz <junielson_tardin_corebiz@whirlpool.com>
2024-05-16 21:11:15 -0300

    Creating coupon section

--- sections/Coupon/Coupon.tsx
+++ sections/Coupon/Coupon.tsx
@@ -1,20 +1,20 @@
 interface Props {
-    coupon: string
-    description: string
+  coupon: string;
+  description: string;
 }
 
 export default function Coupon({
-    coupon,
-    description
+  coupon,
+  description,
 }: Props) {
-    return (
-        <div class="flex flex-col items-center p-6 bg-accent my-5">
-            <p class="p-4 text-2xl bg-white rounded-lg font-bold">
-                {coupon}
-            </p>
-            <p class="mt-4 font-bold">
-                {description}
-            </p>
-        </div>
-    )
+  return (
+    <div class="flex flex-col items-center p-6 bg-accent my-5">
+      <p class="p-4 text-2xl bg-white rounded-lg font-bold">
+        {coupon}
+      </p>
+      <p class="mt-4 font-bold">
+        {description}
+      </p>
+    </div>
+  );
 }
