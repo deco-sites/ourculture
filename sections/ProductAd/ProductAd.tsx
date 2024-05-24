62db12edb738f0de9785e9cbe53b419c88aa9377
junielson_tardin_corebiz <junielson_tardin_corebiz@whirlpool.com>
2024-05-23 21:08:38 -0300

    initial PartialProductAd implementation

--- sections/ProductAd/ProductAd.tsx
+++ sections/ProductAd/ProductAd.tsx
@@ -1,5 +1,5 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
-
+import { JSX } from "preact";
 
 export interface Props {
     product: Product
@@ -11,9 +11,12 @@ interface Product {
     description?: string
     price: string
     imageSrc: ImageWidget
+    href?: string
 }
 
-export default function ProductAd({ product, adDescription }: Props) {
+export type ProductAd = JSX.Element;
+
+export default function ProductAdSection({ product, adDescription }: Props) {
     return (
         <div class="p-2 flex flex-col justify-center items-center relative gap-4 w-fit border border-neutral-500 rounded-md hover:border-accent lg:flex-row">
             <img 
@@ -30,8 +33,13 @@ export default function ProductAd({ product, adDescription }: Props) {
             <div class="flex flex-col gap-3 justify-center items-center lg:h-full lg:justify-end lg:mt-auto lg:items-end">
                 <p class="text-lg font-bold text-accent">{product.price}</p>
                 <div class="flex flex-col gap-3 justify-center items-center lg:flex-row">
-                    <button class="px-6 py-2 w-fit rounded-md border-accent border text-accent hover:bg-accent hover:text-black">Mais Detalhes</button>
-                    <button class="px-6 py-2 w-fit rounded-md border-accent bg-accent border">Comprar</button>
+                    <a 
+                        href={product.href}
+                        class="px-6 py-2 w-fit rounded-md border-accent border no-underline text-accent hover:bg-accent hover:text-black"
+                    >
+                        Mais Detalhes
+                    </a>
+                    <a class="px-6 py-2 w-fit rounded-md border-accent bg-accent border no-underline">Comprar</a>
                 </div>
             </div>
             <button class="rounded-full p-2 flex items-center justify-center absolute top-5 right-5 text-xs text-white bg-accent">
