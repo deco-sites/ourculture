b0d010d18282b21faa3ec99ad2b02d7de612634d
junielson_tardin_corebiz <junielson_tardin_corebiz@whirlpool.com>
2024-05-23 21:08:46 -0300

    initial PartialProductAd implementation

--- sections/PartialProductAd/PartialProductAd.tsx
+++ sections/PartialProductAd/PartialProductAd.tsx
@@ -5,79 +5,82 @@ import { ImageWidget } from "apps/admin/widgets.ts";
 import { usePartialSection } from "deco/hooks/usePartialSection.ts";
 
 export interface Props {
-    productAds: Section<ProductAd>[]
-    message: string
-    image: ImageWidget
-    buttonTitle: string
-    currentProduct?: number
+  productAds: Section<ProductAd>[];
+  message: string;
+  image: ImageWidget;
+  buttonTitle: string;
+  currentProduct?: number;
 }
 
 export function ErrorFallback(_: unknown) {
-    const product = {
-        imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT0yebjaFKFNLElmRSkjxDsSLtBsIDtbfONw&s",
-        title: "Roupas Indianas",
-        description: "Explore nossa coleção exclusiva de roupas indianas",
-        price: "",
-        href: "/culturas"
-    }
+  const product = {
+    imageSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT0yebjaFKFNLElmRSkjxDsSLtBsIDtbfONw&s",
+    title: "Roupas Indianas",
+    description: "Explore nossa coleção exclusiva de roupas indianas",
+    price: "",
+    href: "/culturas",
+  };
 
-    return (
-        <ProductAdComponent 
-            product={product}
-        />
-    )
+  return (
+    <ProductAdComponent
+      product={product}
+    />
+  );
 }
 
 export function LoadingFallback() {
-    const product = {
-        imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9LsYz6T9GWTNlaee8UwtQKZjxxlmGG70xNQmZSnY9dSOGPof0s1O-2Oq8Ldn3-DFu1NI&usqp=CAU",
-        title: "Loading...",
-        description: "Loading...",
-        price: "",
-    }
-    
-    return (
-        <ProductAdComponent 
-            product={product}
-        />
-    )
+  const product = {
+    imageSrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9LsYz6T9GWTNlaee8UwtQKZjxxlmGG70xNQmZSnY9dSOGPof0s1O-2Oq8Ldn3-DFu1NI&usqp=CAU",
+    title: "Loading...",
+    description: "Loading...",
+    price: "",
+  };
+
+  return (
+    <ProductAdComponent
+      product={product}
+    />
+  );
 }
 
 export default function PartialProductAd({
-    productAds = [],
-    message,
-    image,
-    buttonTitle,
-    currentProduct = 0
+  productAds = [],
+  message,
+  image,
+  buttonTitle,
+  currentProduct = 0,
 }: Props) {
-    const productComponent = productAds[currentProduct];
+  const productComponent = productAds[currentProduct];
 
-    return (
-        <div class="flex p-4 gap-4">
-            <productComponent.Component {...productComponent.props} />
+  return (
+    <div class="flex p-4 gap-4">
+      <productComponent.Component {...productComponent.props} />
 
-            <div class="flex flex-col gap-4">
-                <p>
-                    {message}
-                </p>
-                <img
-                    src={image}
-                    width={30}
-                    height={30}
-                >
-                </img>
-                <button 
-                    {...usePartialSection({
-                        props: { 
-                            currentProduct: (currentProduct + 1) > productAds.length ?
-                                0 : currentProduct + 1
-                        }
-                    })}
-                    class="px-5 py-2 bg-accent pointer text-xl font-bold w-full"
-                >
-                    {buttonTitle}
-                </button>
-            </div>
-        </div>
-    )
+      <div class="flex flex-col gap-4">
+        <p>
+          {message}
+        </p>
+        <img
+          src={image}
+          width={30}
+          height={30}
+        >
+        </img>
+        <button
+          {...usePartialSection({
+            props: {
+              currentProduct: (currentProduct + 1) > productAds.length
+                ? 0
+                : currentProduct + 1,
+            },
+          })}
+          class="px-5 py-2 bg-accent pointer text-xl font-bold w-full"
+        >
+          {buttonTitle}
+        </button>
+      </div>
+    </div>
+  );
 }
--- sections/ProductAd/ProductAd.tsx
+++ sections/ProductAd/ProductAd.tsx
@@ -2,49 +2,53 @@ import type { ImageWidget } from "apps/admin/widgets.ts";
 import { JSX } from "preact";
 
 export interface Props {
-    product: Product
-    adDescription?: string
+  product: Product;
+  adDescription?: string;
 }
 
 interface Product {
-    title: string
-    description?: string
-    price: string
-    imageSrc: ImageWidget
-    href?: string
+  title: string;
+  description?: string;
+  price: string;
+  imageSrc: ImageWidget;
+  href?: string;
 }
 
 export type ProductAd = JSX.Element;
 
 export default function ProductAdSection({ product, adDescription }: Props) {
-    return (
-        <div class="p-2 flex flex-col justify-center items-center relative gap-4 w-fit border border-neutral-500 rounded-md hover:border-accent lg:flex-row">
-            <img 
-                width={280}
-                height={420}
-                src={product.imageSrc}
-                decoding="async"
-                loading="lazy"
-            />
-            <div class="flex flex-col gap-3 lg:gap-4 lg:h-full lg:justify-start lg:mb-auto">
-                <p class="text-xl font-bold">{product.title}</p>
-                <p class="text-base">{adDescription ? adDescription : product.description}</p>
-            </div>
-            <div class="flex flex-col gap-3 justify-center items-center lg:h-full lg:justify-end lg:mt-auto lg:items-end">
-                <p class="text-lg font-bold text-accent">{product.price}</p>
-                <div class="flex flex-col gap-3 justify-center items-center lg:flex-row">
-                    <a 
-                        href={product.href}
-                        class="px-6 py-2 w-fit rounded-md border-accent border no-underline text-accent hover:bg-accent hover:text-black"
-                    >
-                        Mais Detalhes
-                    </a>
-                    <a class="px-6 py-2 w-fit rounded-md border-accent bg-accent border no-underline">Comprar</a>
-                </div>
-            </div>
-            <button class="rounded-full p-2 flex items-center justify-center absolute top-5 right-5 text-xs text-white bg-accent">
-                Salvar
-            </button>
+  return (
+    <div class="p-2 flex flex-col justify-center items-center relative gap-4 w-fit border border-neutral-500 rounded-md hover:border-accent lg:flex-row">
+      <img
+        width={280}
+        height={420}
+        src={product.imageSrc}
+        decoding="async"
+        loading="lazy"
+      />
+      <div class="flex flex-col gap-3 lg:gap-4 lg:h-full lg:justify-start lg:mb-auto">
+        <p class="text-xl font-bold">{product.title}</p>
+        <p class="text-base">
+          {adDescription ? adDescription : product.description}
+        </p>
+      </div>
+      <div class="flex flex-col gap-3 justify-center items-center lg:h-full lg:justify-end lg:mt-auto lg:items-end">
+        <p class="text-lg font-bold text-accent">{product.price}</p>
+        <div class="flex flex-col gap-3 justify-center items-center lg:flex-row">
+          <a
+            href={product.href}
+            class="px-6 py-2 w-fit rounded-md border-accent border no-underline text-accent hover:bg-accent hover:text-black"
+          >
+            Mais Detalhes
+          </a>
+          <a class="px-6 py-2 w-fit rounded-md border-accent bg-accent border no-underline">
+            Comprar
+          </a>
         </div>
-    )
+      </div>
+      <button class="rounded-full p-2 flex items-center justify-center absolute top-5 right-5 text-xs text-white bg-accent">
+        Salvar
+      </button>
+    </div>
+  );
 }
