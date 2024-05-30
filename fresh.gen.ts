3f9fc3302158d48716dd221e6aaee1f10ea5b30d
junielson_tardin_corebiz <junielson_tardin_corebiz@whirlpool.com>
2024-05-30 17:27:51 -0300

    creating actions, loaders and components to show events

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -21,9 +21,11 @@ import * as $Header_Searchbar from "./islands/Header/Searchbar.tsx";
 import * as $Newsletter from "./islands/Newsletter.tsx";
 import * as $OutOfStock from "./islands/OutOfStock.tsx";
 import * as $ProductImageZoom from "./islands/ProductImageZoom.tsx";
+import * as $SaveProductButton_SaveProductButton from "./islands/SaveProductButton/SaveProductButton.tsx";
 import * as $SearchControls from "./islands/SearchControls.tsx";
 import * as $ShippingSimulation from "./islands/ShippingSimulation.tsx";
 import * as $ShowMore from "./islands/ShowMore.tsx";
+import * as $ShowProductEvents_ShowProductEvents from "./islands/ShowProductEvents/ShowProductEvents.tsx";
 import * as $WishlistButton_vtex from "./islands/WishlistButton/vtex.tsx";
 import * as $WishlistButton_wake from "./islands/WishlistButton/wake.tsx";
 import { type Manifest } from "$fresh/server.ts";
@@ -51,9 +53,13 @@ const manifest = {
     "./islands/Newsletter.tsx": $Newsletter,
     "./islands/OutOfStock.tsx": $OutOfStock,
     "./islands/ProductImageZoom.tsx": $ProductImageZoom,
+    "./islands/SaveProductButton/SaveProductButton.tsx":
+      $SaveProductButton_SaveProductButton,
     "./islands/SearchControls.tsx": $SearchControls,
     "./islands/ShippingSimulation.tsx": $ShippingSimulation,
     "./islands/ShowMore.tsx": $ShowMore,
+    "./islands/ShowProductEvents/ShowProductEvents.tsx":
+      $ShowProductEvents_ShowProductEvents,
     "./islands/WishlistButton/vtex.tsx": $WishlistButton_vtex,
     "./islands/WishlistButton/wake.tsx": $WishlistButton_wake,
   },
---
+++ islands/SaveProductButton/SaveProductButton.tsx
@@ -0,0 +1,70 @@
+import Modal from "../../components/ui/Modal.tsx";
+import { useSignal } from "@preact/signals";
+import { invoke } from "../../runtime.ts";
+import { useRef } from 'preact/hooks'
+
+export interface Props {
+    title: string
+    productId: string
+}
+
+export default function SaveProductButton({
+    title,
+    productId
+}: Props) {
+    const open = useSignal(false);
+    const textAreaRef = useRef<HTMLTextAreaElement>(null)
+
+    const handleSaveComment = async () => {
+        if(textAreaRef.current?.value) {
+            await invoke.site.actions.comments.postComments({
+                productId: productId,
+                comment: textAreaRef.current?.value
+            })
+        }
+    }
+
+    return (
+        <>
+            <button 
+                class="rounded-full p-2 flex items-center justify-center absolute top-5 right-5 text-xs text-white bg-accent"
+                onClick={() => open.value = true}
+            >
+                {title}
+            </button>
+            <Modal
+                open={open.value}
+                onClose={() => open.value = false}
+            >
+                <div class="flex flex-col p-6">
+                    <div class="flex gap-4">
+                        <img
+                            width={280}
+                            height={420}
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT0yebjaFKFNLElmRSkjxDsSLtBsIDtbfONw&s"
+                            decoding="async"
+                            loading="lazy"
+                        />
+                        <div class="flex flex-col gap-3">
+                            <h2>
+                                Titulo
+                            </h2>
+                            <p>
+                                Descricao
+                            </p>
+                            <textarea ref={textAreaRef} />
+                        </div>
+                    </div>
+                    <div>
+                        <button>
+                            Cancelar
+                        </button>
+                        <button onClick={handleSaveComment}>
+                            Publicar
+                        </button>
+                    </div>
+                </div>                
+            </Modal>
+        </>
+    )
+}
