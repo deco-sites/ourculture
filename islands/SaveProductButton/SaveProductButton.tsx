74b680be26273c99d3bbf00f6c8ed686c8bfba89
junielson_tardin_corebiz <junielson_tardin_corebiz@whirlpool.com>
2024-06-10 20:34:06 -0300

    consolelog

--- islands/SaveProductButton/SaveProductButton.tsx
+++ islands/SaveProductButton/SaveProductButton.tsx
@@ -15,6 +15,8 @@ export default function SaveProductButton({
     const dispatchToast = useSignal(false);
     const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
+    console.log("build")
+
     useSignalEffect(() => {
         if(dispatchToast.value) {
             setTimeout(() => {
